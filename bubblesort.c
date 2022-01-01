9970d3c94de457040335646cf19c0001b11def6d
Stefanos.Stefanou <stefanos.stefanou@gbgplc.com>
2022-01-01 16:35:05 +0000

    Initial

---
+++ bubblesort.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+void main()
+{
+	int copy,counter,counter02,len;
+	scanf("%d",&len);
+	int [10]={
+	for(counter=0;counter<=len-1;counter++){
+		for(counter02=len-1;counter02<=counter;counter--){
+			if (array[counter02] < array[counter02 - 1]) {
+				copy=array[counter];
+				array[counter] = array[counter +1];
+				array[counter +1] = copy;
+			}
+				
+		}
+	}
+}
